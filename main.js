926e2a1bf5dcb26904873c9f8d3f0cad073c094f
manueluriarte21 <80177093+manueluriarte21@users.noreply.github.com>
2021-04-09 21:52:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener ('click', () =>{
+     document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.objetivos', {delay: 700});
+ScrollReveal().reveal('.card-banner-one', {delay: 700});
